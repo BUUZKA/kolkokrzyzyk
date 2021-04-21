7863ff65853be3b786f8e69f133ead6298cb7a31
BUUZKA <72550805+BUUZKA@users.noreply.github.com>
2021-04-21 22:40:25 +0200

    Dodaj pliki projektów.

---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kolkokrzyzyk
+{
+
+    enum Gracz
+    {
+        Krzyzyk,
+        Kolko,
+    }
+    public partial class Form1 : Form
+    {
+        Gracz gracz;
+        public Form1()
+        {
+            InitializeComponent();
+            gracz = Gracz.Krzyzyk;
+            currentPlayerLabel.Text = "Krzyzyk";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Mark(object sender, EventArgs e)
+        {
+            Button senderButton = (Button)sender;
+            if(gracz == Gracz.Krzyzyk)
+            {
+                senderButton.Text = "X";
+                gracz = Gracz.Kolko;
+            }
+            else
+            {
+                senderButton.Text = "O";
+                gracz = Gracz.Krzyzyk;
+            }
+            changeLabel();
+        }
+        public void changeLabel()
+        {
+            if(gracz == Gracz.Krzyzyk)
+            {
+                currentPlayerLabel.Text = "Krzyżyk";
+            }
+            else 
+            {
+                currentPlayerLabel.Text = "Kółko";
+            }
+        }
+    }
+}
